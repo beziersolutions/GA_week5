0d230f4c115ea85384648a96bb9a33046504789c
beziersolutions <andrew.d.robinson@hotmail.co.uk>
2014-02-08 09:59:59 +0000

    Added mouse enter and leave functionality
    not final piece

--- dividedtimes/js/main.js
+++ dividedtimes/js/main.js
@@ -16,6 +16,7 @@
 
 //set at the li element so each ind section can change color
 
+//$(document).ready(function()
 $(function () {
 
     //hides all so form set in the default behaviour
@@ -30,23 +31,43 @@ $(function () {
         console.log('the target is ' + target);
         event.preventDefault(); //prevent the default behavior of the link onclick event
 
-
-
+        //closes focused menu
         if ($(this).hasClass('is-active')) {
-            
+            console.log("if clause entered ");
             console.log('condition is true');
             $(target).slideUp();
             $(this).removeClass('is-active');
             //if class isnt present - do else statement first entry point if is on subsequent navs within session
-
-
         } else { //first entry point removes all class then adds one and hide all secondary navs 
-            $('#primary-nav a').removeClass('is-active');
+            console.log("else clause entered ");
+
             $(this).addClass('is-active');
+
             // $(this).addClass('is-active'); //jquery checks if a class is ative -checks the state (is) and adds a class to html
-            $('#secondary-nav-container section').slideUp();
-            $('#secondary-nav-container').show();
-            $(target).slideDown();
+            //  $(this).parent().css("background", "#666");
+            //   $('#secondary-nav-container section').slideUp();
+
+            //   $('#secondary-nav-container').show();
+
+            //    $(target).slideDown();
+
+
+            $(this).click(function () {
+                
+           
+
+                $('#secondary-nav-container').show();
+$(this).parent().css("background", "#666");
+                $(target).slideDown();
+            });
+
+            //hide submenus on exit
+            $(this).mouseleave(function () {
+                    $('#secondary-nav-container section').slideUp();
+                $(this).parent().removeAttr("background");
+            });
+
+
         }
 
     });
